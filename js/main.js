c84187d08389ae5fc2eb7ca2db94dd8c3e5870b9
Delvin Varghese <me@delv.in>
2015-03-29 22:46:22 +0100

    Creating an efficient for loop to create patterns. Using the 'use' command to create pattern copies.

--- js/main.js
+++ js/main.js
@@ -17,7 +17,6 @@ $(document).ready(function () {
     renderCanvas();
     
     $('.update-shapes').on('change', function() {
-        console.log('Yo'+$(this).val());
         renderCanvas();
     }); 
     
@@ -45,13 +44,40 @@ function renderCanvas(){
     var shapeRotate3 = $( '#rotate-slider-3' ).slider( "value" );
 
     // Tesselation properties
-    var xRepeat = $('#x-repeat').val();
-    var yRepeat = $('#y-repeat').val();
+    var xRepeat = parseInt($('#x-repeat').val());
+    var yRepeat = parseInt($('#y-repeat').val());
+    var numElem = xRepeat * yRepeat;
+    var xScale = 1/xRepeat;
+    var yScale = 1/yRepeat;
+    
+    var shapeFinal1 = drawShape(shapeChoice1, 0, shapeRotate1);
+    var shapeFinal2 = drawShape(shapeChoice2, 1, shapeRotate2);
+    var shapeFinal3 = drawShape(shapeChoice3, 2, shapeRotate3);
 
-    drawShape(shapeChoice1, 0, shapeRotate1);
-    drawShape(shapeChoice2, 1, shapeRotate2);
-    drawShape(shapeChoice3, 2, shapeRotate3);
-
+    var shapeGroup = draw.group();
+    if((xRepeat == 1) && (yRepeat == 1)){
+        shapeGroup.add(shapeFinal1);
+        shapeGroup.add(shapeFinal2);
+        shapeGroup.add(shapeFinal3);
+    } else {
+        shapeFinal1.scale(xScale, yScale);
+        shapeFinal2.scale(xScale, yScale);
+        shapeFinal3.scale(xScale, yScale);
+        
+        shapeGroup.add(shapeFinal1);
+        shapeGroup.add(shapeFinal2);
+        shapeGroup.add(shapeFinal3);
+        for(i=0; i<xRepeat; i++){ 
+            for(j=0; j<yRepeat; j++){
+                console.log('YYY');
+                draw.use(shapeGroup).move(cwidth - cwidth/(i+1), cheight - cheight/(j+1));
+            }
+        }    
+    }
+    
+    
+//    var use  = draw.use(shapeGroup).move(200, 200);
+    
     // Draw required number of objects, allowing for size    
 }
 
@@ -66,88 +92,88 @@ function selectTextarea() {
 // Shape drawing function
 function drawShape(shapeNumber, positionNumber, rotationDegrees){
     
-    var maxSize = 100;
+    var maxSize = 500;
     var padding = 0;
     switch(positionNumber) {
         case 0: // Outer
-            maxSize = 100;
+            maxSize = 500;
             padding = 0;
             break;
         case 1: // Middle
-            maxSize = 75;
-            padding = 12.5;
+            maxSize = 375;
+            padding = 12.5*5;
             break;
         case 2: // Innermost
-            maxSize = 25;
-            padding = 37.5;
+            maxSize = 125;
+            padding = 37.5*5;
             break;
     }
     
     switch(parseInt(shapeNumber)) {
-        case 0: // Circle
-            var circle = draw.circle(0.5).fill('#fff').stroke({ width: 2 }).move(padding,padding).rotate(rotationDegrees);
+        case 0: // Nothing
+            return draw.circle(0.5).fill('#fff').stroke({ width: 2 }).move(padding,padding).rotate(rotationDegrees);
             break;
         case 1: // Circle
-            var circle = draw.circle(maxSize).fill('#fff').stroke({ width: 1 }).move(padding,padding).rotate(rotationDegrees);
+            return draw.defs().circle(maxSize).fill('#fff').stroke({ width: 1 }).move(padding,padding).rotate(rotationDegrees);
             break;
         case 2: // Oval
-            var oval = draw.ellipse(maxSize,maxSize*0.5).fill('#fff').stroke({ width: 1 }).move(padding,padding).rotate(rotationDegrees);
+            return draw.ellipse(maxSize,maxSize*0.5).fill('#fff').stroke({ width: 1 }).move(padding,padding).rotate(rotationDegrees);
             break;
         case 3: // Square
-            var square = draw.rect(maxSize,maxSize).fill('#fff').stroke({ width: 1 }).move(padding,padding).rotate(rotationDegrees);
+            return draw.defs().rect(maxSize,maxSize).fill('#fff').stroke({ width: 1 }).move(padding,padding).rotate(rotationDegrees);
             break;
         case 4: // Triangle
-            var triangle = draw.polygon([[0,maxSize], [maxSize,maxSize], [maxSize*0.5,0]]).fill('#fff').stroke({ width: 1 }).move(padding,padding).rotate(rotationDegrees);
+            return draw.polygon([[0,maxSize], [maxSize,maxSize], [maxSize*0.5,0]]).fill('#fff').stroke({ width: 1 }).move(padding,padding).rotate(rotationDegrees);
             break;
         case 5: // Rectangle
-            var rectangle = draw.rect(maxSize,0.8*maxSize).fill('#fff').stroke({ width: 1 }).move(padding,padding).rotate(rotationDegrees);
+            return draw.rect(maxSize,0.8*maxSize).fill('#fff').stroke({ width: 1 }).move(padding,padding).rotate(rotationDegrees);
             break;
         case 6: // Pentagon
-            var pentagon = draw.polygon([[0,0.5*maxSize], [0.5*maxSize,0], 
+            return draw.polygon([[0,0.5*maxSize], [0.5*maxSize,0], 
                                          [maxSize,0.5*maxSize], [0.75*maxSize,maxSize], 
                                          [0.25*maxSize,maxSize]]).fill('#fff')
             .stroke({ width: 1 }).move(padding,padding).rotate(rotationDegrees);
             break;
         case 7: // Hexagon
-            var hexagon = draw.polygon([[0,0.5*maxSize], [0.25*maxSize,0], 
+            return draw.polygon([[0,0.5*maxSize], [0.25*maxSize,0], 
                                         [0.75*maxSize,0], [maxSize,0.5*maxSize], 
                                         [0.75*maxSize,maxSize], [0.25*maxSize,maxSize]])
             .fill('#fff').stroke({ width: 1 }).move(padding,padding).rotate(rotationDegrees);
             break;
         case 8: // Diamonds
-            var diamond = draw.circle(maxSize);
+            return draw.circle(maxSize);
             break;
         case 9: // Flower
-            var flower = draw.circle(maxSize);
+            return draw.circle(maxSize);
             break;
         case 10: // Starfish
-            var starfish = draw.circle(maxSize);
+            return draw.circle(maxSize);
             break;
         case 11: // Star
-            var star = draw.polygon([[0,0], [0.5*maxSize,0.25*maxSize], 
+            return draw.defs().polygon([[0,0], [0.5*maxSize,0.25*maxSize], 
                                      [maxSize,0], [0.75*maxSize,0.5*maxSize], 
                                      [maxSize,maxSize], [0.5*maxSize,0.75*maxSize], 
                                      [0,maxSize], [0.25*maxSize,0.5*maxSize]])
             .fill('#fff').stroke({ width: 1 }).move(padding,padding).rotate(rotationDegrees);
             break;
         case 12: // Plus
-            var plus = draw.polygon([[0.4*maxSize,0], [0.6*maxSize,0], [0.6*maxSize,0.4*maxSize], 
+            return draw.polygon([[0.4*maxSize,0], [0.6*maxSize,0], [0.6*maxSize,0.4*maxSize], 
                                      [maxSize,0.4*maxSize], [maxSize,0.6*maxSize], [0.6*maxSize,0.6*maxSize], 
                                      [0.6*maxSize,maxSize], [0.4*maxSize,maxSize], [0.4*maxSize,0.6*maxSize], 
                                      [0,0.6*maxSize], [0,0.4*maxSize], [0.4*maxSize,0.4*maxSize]])
             .fill('#fff').stroke({ width: 1 }).move(padding,padding).rotate(rotationDegrees);
             break;
         case 13: // Cross
-            var cross = draw.circle(maxSize);
+            return draw.circle(maxSize);
             break;
         case 14: // Arrows
-            var arrows = draw.circle(maxSize);
+            return draw.circle(maxSize);
             break;
         case 15: // Flag
-            var flag = draw.circle(maxSize);
+            return draw.circle(maxSize);
             break;
         case 16: // Heart
-            var heart = draw.circle(maxSize);
+            return draw.circle(maxSize);
             break;
     }
 }
